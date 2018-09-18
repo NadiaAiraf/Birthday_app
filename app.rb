194e68250f7d77f56b19627c4afe20eb97ea663d
NadiaAiraf <aidanfaria@gmail.com>
2018-09-18 18:03:43 +0100

    ITS ALLIIIIIIVVVVVVVEEEEE

--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
 
+enable :sessions
+set :session_secret, "my secret session"
+
 get '/' do
+  p params
   erb(:home)
+end
+
+post '/birthday' do
+  p params
+  session[:name] = params[:fname]
+  session[:date] = params[:bdate]
+  redirect '/birthday'
+end
+
+get '/birthday' do
+  p session
+  erb(:birthday)
 end
